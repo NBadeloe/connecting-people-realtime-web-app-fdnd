1559c7168b42025b5fe52019bf369b61c80a2e15
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2022-06-06 17:46:44 +0200

    chat code changed

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,135 +1,50 @@
-const socket = io()
-const dom = {
-    nameInput: document.querySelector('.name-input'),
-    joinButton: document.querySelector('.join'),
-    inputAvatar: document.querySelector('.messaging-form .avatar'),
-    welcomeMessage: document.querySelector('.title_app'),
-    feed: document.querySelector('.feed'),
-    feedback: document.querySelector('.feedback')
-    
-};
+let socket = io()
+let messageContainer = document.querySelector('section ul')
+let input = document.querySelector('input')
+let form = document.querySelector('form')
+let message = document.querySelector('li')
 
-const user = {
-    name: null,
-    avatar: null
-};
 
-const getAvatar = () => {
-    const size = Math.floor(Math.random() * 100) + 25;
+// let userName= prompt(' What is your name')
+// appendMessage('You joined')
+// socket.emit('new-user', userName)
 
-    return `url(https://www.placecage.com/${size}/${size})`;
-};
+form.addEventListener('submit', (e) =>{
+  e.preventDefault()
+  let message = input.value
+  appendMessage(`You: ${message}`)
+  socket.emit('send-chat-message', message)
+  input.value = ''
 
-const addEntry = ({ user, message }, you) => {
-    const entry = document.createElement('li');
-    const date = new Date();
+})
 
-    entry.classList = `message-entry${you ? ' message-entry-own' : ''}`
-    entry.innerHTML = `
-        <span class="avatar" style="background: ${user.avatar}; background-size: contain;"></span>
-        <div class="message-body">
-            <span class="user-name">${you ? 'You' : user.name}</span>
-            <time>@ ${date.getHours()}:${date.getMinutes()}</time>
-            <p>${message}</p>
-        </div>
-    `;
+socket.on('chat-message', data =>{
+  appendMessage(`${data.userName}:${data.message}`)
+})
 
-    dom.feed.appendChild(entry);
-};
+socket.on('user-connected', (userName) => {
+  appendMessage(`${userName} has connected`)
+})
 
-const addWelcomeMessage = (user, you) => {
-    const welcomeMessage = document.createElement('li');
-    const message = you ?
-        'You have joined the post it board' :
-        `<span class="user-name">${user.name}</span> has joined the post it board`;
-    
-    const avatar = you ? '' : `<span class="avatar" style="background: ${user.avatar}; background-size: contain;"></span>`;
 
-    welcomeMessage.classList = 'welcome-message';
-    welcomeMessage.innerHTML = `
-        <div class="welcome-message-text">
-            ${avatar}
-            ${message}
-        </div>
-    `;
-
-    dom.feed.appendChild(welcomeMessage);
-    
-};
-
-const enterChannel = () => {
-    const avatar = getAvatar();
-    const name = dom.nameInput.value;
-
-    // dom.joinButton.remove();
-    dom.welcomeMessage.remove();
-
-    dom.nameInput.value = '';
-    dom.nameInput.placeholder = 'Send a message for the channel...';
-
-    dom.inputAvatar.innerText = '';
-    dom.inputAvatar.style.backgroundImage = avatar;
-    dom.inputAvatar.style.backgroundSize = 'contain';
-
-    user.name = name;
-    user.avatar = avatar;
-
-    addWelcomeMessage({ avatar }, true);
-
-    socket.emit('user connected', {
-        name,
-        avatar
-    });
-};
-
-socket.on('user connected', payload => addWelcomeMessage(payload, false));
-
-socket.on('user typing', ({ user, typers }) => {
-    dom.feedback.innerHTML = typers > 1 ? 'Several people are typing' : `<i>${user}</i> is typing`;
-});
-
-socket.on('user stopped typing', typers => {
-    if (!typers) {
-        dom.feedback.innerHTML = '';
-    }
-});
-
-socket.on('send message', payload => {
-    addEntry(payload);
-
-    if (!payload.typers) {
-        dom.feedback.innerHTML = '';
-    }
-});
-
-dom.joinButton.onclick = e => {
-    e.preventDefault();
-
-    if (!dom.nameInput.value) {
-        dom.nameInput.parentElement.classList.add('error');
-    } else {
-        enterChannel();
-
-        dom.nameInput.onkeyup = e => {
-            socket.emit('user typing');
-    
-            // If user presses enter
-            if (e.keyCode === 13) {
-                const message = e.target.value;
-
-                socket.emit('send message', {
-                    message,
-                    user
-                });
-
-                addEntry({ user, message }, true);
-    
-                e.target.value = '';
-            }
-    
-            if (e.target.value === '') {
-                socket.emit('user stopped typing');
-            }
-        };
-    }
+function appendMessage(message) {
+  let messageElement = document.createElement('li')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
 }
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault()
+//   if (input.value) {
+//     socket.emit('message', input.value)
+//     input.value = ''
+//   }
+// })
+
+
+// socket.on('message', message => {
+//   messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }))
+//   messages.scrollTop = messages.scrollHeight
+// })
+
+//functions//
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
+const path = require("path");
 const io = require("socket.io")(http);
 const port = 4242;
+const users = {}
 
-
-app.use(express.static("public"));
+app.use(express.static(path.resolve("public")));
 
 // templating //
 app.set("view engine", "ejs");
@@ -15,49 +16,34 @@ app.get("/", (req, res) => {
 	res.render("index");
 });
 
-// Port //
+//sockets //
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected')
+
+//   socket.on('message', (message) => {
+//     io.emit('message', message)
+//   })
+
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected')
+//   })
+// })
+
+io.on('connection', socket =>{
+  socket.on('new-user', (userName) => {
+    users[socket.id] = userName
+    socket.broadcast.emit('user-connected', userName)
+
+})
+socket.on('send-chat-message', message =>{
+ socket.broadcast.emit('chat-message',{ message: message, userName: users[socket.id]
+  })
+ })
+})
+
+
+// port //
 http.listen(process.env.PORT || 4242, () =>
-console.log(`App avaialble on http://localhost:4242`)
+	console.log(`App avaialble on http://localhost:4242`)
 )
-
-const users = {};
-const typers = {}
-
-io.on('connection', socket => {
-    console.log('connected...');
-
-    socket.on('user connected', payload => {
-        users[socket.id] = {
-            id: socket.id,
-            name: payload.name,
-            avatar: payload.avatar
-        };
-
-        socket.broadcast.emit('user connected', users[socket.id]);
-    });
-
-    socket.on('user typing', () => {
-        typers[socket.id] = 1;
-
-        socket.broadcast.emit('user typing', {
-            user: users[socket.id].name,
-            typers: Object.keys(typers).length
-        });
-    });
-
-    socket.on('user stopped typing', () => {
-        delete typers[socket.id];
-
-        socket.broadcast.emit('user stopped typing', Object.keys(typers).length);
-    });
-
-    socket.on('send message', payload => {
-        delete typers[socket.id];
-
-        socket.broadcast.emit('send message', {
-            user: payload.user,
-            message: payload.message,
-            typers: Object.keys(typers).length
-        });
-    });
-});
