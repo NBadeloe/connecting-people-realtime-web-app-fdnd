3c0cd3f82f092024242cd6de541d7a4e7a7f4394
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-06-08 14:25:10 +0200

    draggable post its

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -3,6 +3,7 @@ let messageContainer = document.querySelector('section ul')
 let input = document.querySelector('input')
 let form = document.querySelector('form')
 let message = document.querySelector('li')
+var isDown = false;
 
 
 // let userName= prompt(' What is your name')
@@ -26,25 +27,52 @@ socket.on('user-connected', (userName) => {
   appendMessage(`${userName} has connected`)
 })
 
+let current = null
+let offset = [0,0]; 
+let mousePosition;
+let row = -1;
+let column = 0
 
 function appendMessage(message) {
+  
   let messageElement = document.createElement('li')
   messageElement.innerText = message
+  messageElement.addEventListener('mousedown', function(e) {
+    isDown = true;
+    current = e.target
+    offset = [
+        current.offsetLeft - e.clientX,
+        current.offsetTop - e.clientY
+    ];
+  }, true);
+  
+  document.addEventListener('mouseup', function() {
+    isDown = false;
+    current = null
+  }, true);
+
+
+  if((messageContainer.children.length) % 5 === 0) {
+    row++
+    column = 0
+  }
+  messageElement.style.top = (14 * row) + 'em'
+  messageElement.style.left = ((13 * column) + 1) + 'em'
+  column++
   messageContainer.append(messageElement)
 }
 
-// form.addEventListener('submit', event => {
-//   event.preventDefault()
-//   if (input.value) {
-//     socket.emit('message', input.value)
-//     input.value = ''
-//   }
-// })
+document.addEventListener('mousemove', function(event) {
+  event.preventDefault();
+  if (isDown) {
+      mousePosition = {
+  
+          x : event.clientX,
+          y : event.clientY
+  
+      };
+      current.style.left = (mousePosition.x + offset[0]) + 'px';
+      current.style.top  = (mousePosition.y + offset[1]) + 'px';
+  }
+}, true);
 
-
-// socket.on('message', message => {
-//   messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }))
-//   messages.scrollTop = messages.scrollHeight
-// })
-
-//functions//
